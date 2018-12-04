54409eb1166b15db8775c0341c9749ebc48a9b5f
Muhammad Eko Jumaddin <ekojumaddin@gmail.com>
2018-12-04 11:24:31 +0800

    Search date di item by eko

---
+++ Bootcamp20.API.BussinessLogic/Interface/Master/ItemService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bootcamp20.API.DataAccess.Models;
+using Bootcamp20.API.DataAccess.Param;
+using Bootcamp20.API.Common.Interface;
+
+namespace Bootcamp20.API.BussinessLogic.Interface.Master
+{
+    public class ItemService : IItemService
+    {
+        private readonly IItemRepository _itemRepository;
+        public ItemService() { }
+        public ItemService(IItemRepository itemRepository)
+        {
+            this._itemRepository = itemRepository;
+        }
+
+        public bool Delete(int? id)
+        {
+            return _itemRepository.Delete(id);
+        }
+
+        public List<Item> Get()
+        {
+            return _itemRepository.Get();
+        }
+
+        public Item Get(int? id)
+        {
+            return _itemRepository.Get(id);
+        }
+
+        public List<Item> GetName(ItemParam _itemparam)
+        {
+            return _itemRepository.GetName(_itemparam);
+        }
+
+        public bool Insert(ItemParam _itemparam)
+        {
+            return _itemRepository.Insert(_itemparam);
+        }
+
+        public List<Item> Search(string name)
+        {
+            return _itemRepository.Search(name);
+        }
+
+        public bool Update(ItemParam _itemparam)
+        {
+            return _itemRepository.Update(_itemparam);
+        }
+    }
+}
